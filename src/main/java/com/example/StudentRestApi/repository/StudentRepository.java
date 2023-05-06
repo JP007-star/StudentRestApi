e8d13edd4f5c2edbb4f88037471434eacb72032f
JP <justice_of_peace@yahoo.com>
2023-05-06 22:54:34 +0530

    cross orgin fix

--- src/main/java/com/example/StudentRestApi/repository/StudentRepository.java
+++ src/main/java/com/example/StudentRestApi/repository/StudentRepository.java
@@ -1,7 +1,6 @@
 package com.example.StudentRestApi.repository;
-
-import com.example.StudentRestApi.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.StudentRestApi.entity.Student;   /// --> user defined packages
+import org.springframework.data.jpa.repository.JpaRepository;   /// --> build in package or other party package
 
 public interface StudentRepository extends JpaRepository <Student, Integer> {
 }
